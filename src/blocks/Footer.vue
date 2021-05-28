165189dc6dafe4d0202a6f83858b801707ec58b4
behindloader <simonsuprun@gmail.com>
2021-05-28 16:08:49 +0300

    Add tg link

--- src/blocks/Footer.vue
+++ src/blocks/Footer.vue
@@ -7,6 +7,7 @@
       </div>
       <div class="column">
         <a href="https://vk.com/donkey_engine" target="_blank"><button class="button is-light"><i class="fab fa-vk fa-lg"></i></button></a>
+        <a href="https://t.me/donkey_engine" target="_blank"><button class="button is-light"><i class="fab fa-telegram fa-lg"></i></button></a>
       </div>
     </div>
   </footer>
