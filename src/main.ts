b576edaa37f31b016d2a464350d0946ff4f045be
tmcgroul <safonov.a.r@mail.ru>
2021-07-29 01:09:03 +0300

    Add discord auth

--- src/main.ts
+++ src/main.ts
@@ -10,49 +10,46 @@ import { getCookie } from './utils'
 import 'bulma/css/bulma.css'
 import './main.css'
 
-const app = createApp({
-  created() {
-    const user = localStorage.getItem('user')
-    if (user) {
-      try {
-        store.commit({
-          type: 'setUser',
-          user: JSON.parse(user)
-        })
-      } catch (err) {
-        localStorage.removeItem('user')
-        store.commit('removeUser')
+async function main() {
+  axios.defaults.baseURL = settings.API_BASE_URL
+  try {
+    const { data } = await axios.get('/me/')
+    store.commit({
+      type: 'setUser',
+      user: data
+    })
+  } finally {
+    const app = createApp({
+      render() {
+        return h(App)
       }
-    }
-  },
-  render() {
-    return h(App)
+    })
+    axios.interceptors.request.use(config => {
+      if (!config.method) return config
+      if (['post', 'put', 'patch', 'delete'].includes(config.method)) {
+        const token = getCookie('csrftoken')
+        config.headers[config.method]['X-CSRFToken'] = token
+      }
+      return config
+    })
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 403) {
+          store.commit('removeUser')
+          return Promise.reject(error)
+        }
+        return Promise.reject(error)
+      }
+    )
+    app.config.globalProperties.$http = axios
+    app.use(router)
+    app.use(store)
+    app.mount('#app')
   }
-})
-axios.defaults.baseURL = settings.API_BASE_URL
-axios.interceptors.request.use(config => {
-  if (!config.method) return config
-  if (['post', 'put', 'patch', 'delete'].includes(config.method)) {
-    const token = getCookie('csrftoken')
-    config.headers[config.method]['X-CSRFToken'] = token
-  }
-  return config
-})
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 403) {
-      localStorage.removeItem('user')
-      store.commit('removeUser')
-      return Promise.reject(error)
-    }
-    return Promise.reject(error)
-  }
-)
-app.config.globalProperties.$http = axios
-app.use(router)
-app.use(store)
-app.mount('#app')
+}
+
+main()
 
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
